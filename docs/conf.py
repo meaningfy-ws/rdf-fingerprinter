aec76e760f76b2acaf201abbf3ade396008507b3
lps <costezki.eugen@gmail.com>
2020-08-15 16:27:58 +0200

    update docs/config.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,17 +10,36 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
+import re
 
 project = 'rdf-fingerprinter'
 copyright = '2020, Eugeniu Costetchi'
 author = 'Eugeniu Costetchi'
 
+
+# Find version. We have to do this because we can't import it in Python 3 until
+# its been automatically converted in the setup process.
+def find_version(filename):
+    _version_re = re.compile(r'__version__ = "(.*)"')
+    for line in open(filename):
+        version_match = _version_re.match(line)
+        if version_match:
+            return version_match.group(1)
+
+
+# The full version, including alpha/beta/rc tags.
+release = find_version("../fingerprint/__init__.py")
+# The short X.Y version.
+version = re.sub("[0-9]+\\.[0-9]\\..*", "\1", release)
+
+# The encoding of source files.
+source_encoding = "utf-8"
 
 # -- General configuration ---------------------------------------------------
 
@@ -28,7 +47,26 @@ author = 'Eugeniu Costetchi'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    # "myst_parser",
+    "sphinxcontrib.apidoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
 ]
+
+apidoc_module_dir = "../fingerprint"
+apidoc_output_dir = "srcdocs"
+add_module_names = False
+autodoc_default_options = {
+    'members': None,
+    'member-order': 'bysource',
+    # 'special-members': True,
+    'undoc-members': None,
+    "private-members": None,
+    # 'exclude-members': '__weakref__'
+}
+
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -36,8 +74,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "draft"]
 
 # -- Options for HTML output -------------------------------------------------
 
