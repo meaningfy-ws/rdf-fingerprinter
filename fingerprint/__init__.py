fc39bc53bfd5d6b10fd5910ac6c9b125248ed7de
Mihai <coslet.mihai@gmail.com>
2020-12-11 14:04:58 +0200

    Add logging configuration from file.

--- fingerprint/__init__.py
+++ fingerprint/__init__.py
@@ -5,18 +5,12 @@
 # Author: Eugeniu Costetchi
 # Email: costezki.eugen@gmail.com
 
-import logging
+import logging.config
+from json import load
 
 __docformat__ = "restructuredtext en"
 
 __version__ = "0.2.5"
 __date__ = "2020-12-11"
 
-# hard coding the log level and format
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '%(asctime)s %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logging.config.dictConfig(load(open('logging_config.json')))
